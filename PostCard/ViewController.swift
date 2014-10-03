3bd918084cd9419dbeda4917dc065c09c6d67195
PHYLLIS HOLLINGSHEAD <phyllis@PHYLLIS-HOLLINGSHEADs-MacBook.local>
2014-10-03 15:43:27 -0500

    first rev of project

--- PostCard/ViewController.swift
+++ PostCard/ViewController.swift
@@ -10,16 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var MessageLabel: UILabel!
+    
+    @IBOutlet weak var NameTextBox: UITextField!
+
+    @IBOutlet weak var MessageTextBox: UITextField!
+    
+    @IBOutlet var SendButtonControl: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
+    @IBAction func SendButton(sender: UIButton) {
+        
+        MessageLabel.hidden = false
+        MessageLabel.text = "Hello " + NameTextBox.text + " " + MessageTextBox.text
+        NameTextBox.text = ""
+        MessageTextBox.text = ""
+        MessageTextBox.resignFirstResponder()
+        MessageLabel.textColor = UIColor.redColor()
+        SendButtonControl.setTitle("Mail Sent", forState:UIControlState.Normal)
+        var answer = calcAge(2)
+        MessageTextBox.text = "age " + "\(answer)"
+        print(answer)
+        
+    }
+    
+    func calcAge(age: Int) -> Int
+    {
+        
+        let age = age + 2
+        return age
+    }
+    
+    
 }
 
